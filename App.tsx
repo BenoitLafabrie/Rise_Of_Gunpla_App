f6336ff4f04c70c73bc2ab224468b83b9aac9b18
BenoitLafabrie <[benoit_lafabrie_pro@fastmail.com](mailto:benoit_lafabrie_pro@fastmail.com)>
2023-08-14 20:55:43 +0200

    Placeholder test

--- App.tsx
+++ App.tsx
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+const placeholderImage = require("./assets/Placeholder_image.webp");
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <View>
+        <Image source={placeholderImage} resizeMode="contain" />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
