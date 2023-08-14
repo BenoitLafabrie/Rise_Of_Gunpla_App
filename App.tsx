b3e13973a039ea39783a46045c136e744224850a
BenoitLafabrie <[benoit_lafabrie_pro@fastmail.com](mailto:benoit_lafabrie_pro@fastmail.com)>
2023-08-14 21:05:26 +0200

    First commit

--- App.tsx
+++ App.tsx
@@ -1,24 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import * as React from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
 
 const placeholderImage = require("./assets/Placeholder_image.webp");
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View>
-        <Image source={placeholderImage} resizeMode="contain" />
+    <NavigationContainer>
+      <View style={styles.container}>
+        <View>
+          <Image source={placeholderImage} resizeMode="contain" />
+        </View>
+        <StatusBar style="auto" />
       </View>
-      <StatusBar style="auto" />
-    </View>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
