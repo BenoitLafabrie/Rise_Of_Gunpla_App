9980bf2101fa66b14235697162317c60ed076ce7
BenoitLafabrie <[benoit_lafabrie_pro@fastmail.com](mailto:benoit_lafabrie_pro@fastmail.com)>
2023-08-15 14:14:35 +0200

    Bottom navigation prep done with placeholder screens

--- App.tsx
+++ App.tsx
@@ -1,28 +1,96 @@
 import * as React from "react";
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Image } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
+import { MaterialIcons } from "@expo/vector-icons";
+import HomeScreen from "./Screens/HomeScreen";
+import ExploreScreen from "./Screens/ExploreScreen";
+import CartScreen from "./Screens/CartScreen";
+import WishlistScreen from "./Screens/WishlistScreen";
+import AccountScreen from "./Screens/AccountScreen";
 
-const placeholderImage = require("./assets/Placeholder_image.webp");
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <View style={styles.container}>
-        <View>
-          <Image source={placeholderImage} resizeMode="contain" />
-        </View>
-        <StatusBar style="auto" />
-      </View>
+      <Tab.Navigator
+        screenOptions={{
+          tabBarActiveTintColor: "#FB0041",
+          tabBarInactiveTintColor: "#151515",
+        }}
+      >
+        <Tab.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            tabBarLabel: "Home",
+            tabBarIcon: ({ focused }) => (
+              <Ionicons
+                name="ios-home-sharp"
+                size={32}
+                color={focused ? "#FB0041" : "#151515"}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Explore"
+          component={ExploreScreen}
+          options={{
+            tabBarLabel: "Explore",
+            tabBarIcon: ({ focused }) => (
+              <Ionicons
+                name="ios-search"
+                size={32}
+                color={focused ? "#FB0041" : "#151515"}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Cart"
+          component={CartScreen}
+          options={{
+            tabBarLabel: "Cart",
+            tabBarIcon: ({ focused }) => (
+              <Ionicons
+                name="ios-cart"
+                size={32}
+                color={focused ? "#FB0041" : "#151515"}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Wishlist"
+          component={WishlistScreen}
+          options={{
+            tabBarLabel: "Wishlist",
+            tabBarIcon: ({ focused }) => (
+              <MaterialIcons
+                name="favorite"
+                size={32}
+                color={focused ? "#FB0041" : "#151515"}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Account"
+          component={AccountScreen}
+          options={{
+            tabBarLabel: "Account",
+            tabBarIcon: ({ focused }) => (
+              <MaterialIcons
+                name="account-circle"
+                size={32}
+                color={focused ? "#FB0041" : "#151515"}
+              />
+            ),
+          }}
+        />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
---
+++ Screens/WishlistScreen.tsx
@@ -0,0 +1,24 @@
+import { Image, StyleSheet, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import * as React from "react";
+
+const placeholderImage = require("../assets/Placeholder_image.webp");
+export default function WishlistScreen() {
+  return (
+    <View style={styles.container}>
+      <View>
+        <Image source={placeholderImage} resizeMode="contain" />
+      </View>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
