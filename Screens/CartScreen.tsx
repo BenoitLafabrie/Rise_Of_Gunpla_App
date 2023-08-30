4b7ae14986a699796235851280d05348529ade3a
BenoitLafabrie <[benoit_lafabrie_pro@fastmail.com](mailto:benoit_lafabrie_pro@fastmail.com)>
2023-08-30 15:15:59 +0200

    Tab displayed when no items in cart done

--- Screens/CartScreen.tsx
+++ Screens/CartScreen.tsx
@@ -1,24 +1,28 @@
-import { Image, StyleSheet, Text, View } from "react-native";
-import { StatusBar } from "expo-status-bar";
+import {Image, StyleSheet, Text, View} from "react-native";
+import {StatusBar} from "expo-status-bar";
 import * as React from "react";
-export default function CartScreen() {
-  return (
-    <View style={styles.container}>
-      <View>
-        <Text style={{ fontFamily: "Lato_400Regular", fontSize: 32 }}>
-          Cart
-        </Text>
-      </View>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+export default function CartScreen({ navigation }) {
+    return (
+        <View style={styles.container}>
+            <View>
+                <Text style={{fontFamily: "Lato_400Regular", fontSize: 30}}>
+                    No orders yet,
+                </Text>
+                <Text onPress={() => navigation.navigate("Home")} style={{fontFamily: "Lato_400Regular", fontSize: 30, color: "#FB0041"}}>let's go shopping!
+                </Text>
+            </View>
+            <StatusBar style="auto"/>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
+    container: {
+        flex: 1,
+        flexDirection: "row",
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
 });
