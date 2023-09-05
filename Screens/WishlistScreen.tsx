cc00d9bcc172b8fe3d1e0b30a71908eb5a709713
BenoitLafabrie <[benoit_lafabrie_pro@fastmail.com](mailto:benoit_lafabrie_pro@fastmail.com)>
2023-09-05 16:53:50 +0200

    Tab displayed when no items wishlisted done

--- Screens/WishlistScreen.tsx
+++ Screens/WishlistScreen.tsx
@@ -1,13 +1,25 @@
-import { Image, StyleSheet, Text, View } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import * as React from "react";
-export default function WishlistScreen() {
+
+export default function WishlistScreen({ navigation }) {
   return (
     <View style={styles.container}>
       <View>
-        <Text style={{ fontFamily: "Lato_400Regular", fontSize: 32 }}>
-          My wishlist
+        <Text style={{ fontFamily: "Lato_400Regular", fontSize: 30 }}>
+          No favorites yet...
         </Text>
+        <Pressable onPress={() => navigation.navigate("Home")}>
+          <Text
+            style={{
+              fontFamily: "Lato_400Regular",
+              fontSize: 30,
+              color: "#FB0041",
+            }}
+          >
+            Are you sure?
+          </Text>
+        </Pressable>
       </View>
       <StatusBar style="auto" />
     </View>
@@ -17,6 +29,7 @@ export default function WishlistScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: "row",
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
